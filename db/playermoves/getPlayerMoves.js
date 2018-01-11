e37f225bd5637e0fda463557b644075899f3ee71
Gerren Penaloza <residualgp@gmail.com>
2018-01-10 22:29:03 -0800

    landing page is now a log in screen, starting on the front end work for the log in feature

--- db/playermoves/getPlayerMoves.js
+++ db/playermoves/getPlayerMoves.js
@@ -1,11 +1,5 @@
 const database = require("../index");
-// monsters.readMonster = function( cardPK ) {
-// 	return database.one( 
-// 'SELECT * FROM "Monsters" 
-// INNER JOIN "Doors" ON "Monsters"."doorID_FK" = "Doors"."doorsID" 
-// INNER JOIN "Cards" ON "Cards"."cardsID" = "Doors"."cardID_FK" 
-// WHERE "cardsID" = $1', [cardPK] );
-// };
+
 const PLAYER_MOVES_JOIN_QUERY = 'SELECT * FROM playermoves WHERE playerfk = $1';
 
 const getPlayerMoves = playersfk => {
